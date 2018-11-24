f7ff2be6e51ae6f0154187e5ebeedb0a3b36da4f
Donald Pakkies <donaldpakkies@gmail.com>
2018-11-24 14:38:39 +0200

    Feat: convert all cookies into properties

--- Cookies/Cookies.php
+++ Cookies/Cookies.php
@@ -19,5 +19,9 @@ class Cookies
   public function __construct()
   {
     $this->all = $_COOKIE;
+
+    foreach ($this->all as $key => $value) {
+      if ($key !== 'all') $this->{strtolower($key)} = $value;
+    }
   }
 }
